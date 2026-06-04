efc61417b4695bfbeda9b7cd6824b20f1f4fd92b
Lionel <llsalinas977@gmail.com>
2026-06-04 20:30:26 -0300

    set-up the basic html5 environment

---
+++ src/main.js
@@ -0,0 +1,11 @@
+console.log("Micropolis Engine Initializing...");
+
+// later on:
+// import { TileConstants } from './engine/tileConstants.js';
+// import { CityLocation } from './engine/cityLocation.js';
+
+function initSandbox() {
+    console.log("Sandbox ready.");
+}
+
+initSandbox();
